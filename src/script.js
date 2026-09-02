98100edf51cedce6b70d6742e620beb59b254334
gacekk33 <collab.gacek2@gmail.com>
2026-09-02 19:38:53 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,10 +6,18 @@ tabs.forEach(tab => {
         tab.classList.add("active");
     });
 });
+
 const newTabButton = document.querySelector(".new-tab");
+
 newTabButton.addEventListener("click", () => {
     const newTab = document.createElement("div");
     newTab.classList.add("tab");
+
+    newTab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("active"));
+        newTab.classList.add("active");
+    });
+
     newTab.textContent = "Nowa karta";
 
     document.querySelector(".tabs").insertBefore(newTab, newTabButton);
